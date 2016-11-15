27a60c2e717e6aef33038809a9e22ce2ddb8f9dd
Marina Chatton <marina.chatton@gmail.com>
2016-11-15 16:33:40 +0100

    ADD Picasso + image
    added Picasso dependency and loaded an image in PicassoActivity from
    Internet

--- app/src/main/java/com/chatton/marina/holdall/PicassoActivity.java
+++ app/src/main/java/com/chatton/marina/holdall/PicassoActivity.java
@@ -2,12 +2,19 @@ package com.chatton.marina.holdall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class PicassoActivity extends AppCompatActivity {
+    String url= "http://francodex.com/wp-content/uploads/2015/08/Chaton851426651.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picasso);
+
+        ImageView picassoImage = (ImageView) findViewById(R.id.image_picasso);
+        Picasso.with(this).load(url).into(picassoImage);
     }
 }
